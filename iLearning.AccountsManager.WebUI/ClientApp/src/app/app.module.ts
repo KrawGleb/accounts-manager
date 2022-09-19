475a89168d165d7f809629a5bb34f4b6ec03f183
KrawGleb <krawcevitsch@gmail.com>
2022-09-20 01:29:41 +0300

    UI common components

--- iLearning.AccountsManager.WebUI/ClientApp/src/app/app.module.ts
+++ iLearning.AccountsManager.WebUI/ClientApp/src/app/app.module.ts
@@ -5,30 +5,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ComponentsModule } from './components/components.module';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { RegistrationComponent } from './modules/auth/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegistrationComponent },
+    ]),
+    ComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
